6d63b5a0ff11a157141ec8c742a823ccca2d2291
RumelNHORS <rumelnhors52@gmail.com>
2024-03-14 16:11:47 +0600

    import all the models into the admin.py file.

--- asset_tracking/admin.py
+++ asset_tracking/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from asset_tracking import models
 
-# Register your models here.
+# Register models here.
+admin.site.register(models.Company)
+admin.site.register(models.Employee)
+admin.site.register(models.Device)
+admin.site.register(models.DeviceLog)
